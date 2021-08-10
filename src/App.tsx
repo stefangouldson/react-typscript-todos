ddc7fb15e700a09bbacc271d1460a02086c083bb
Stefan Gouldson <stefan@Stefans-MacBook-Pro-Cool-Studio.local>
2021-08-10 15:50:08 +0100

    can add todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
+
 import Todos from './components/Todos';
 import Todo from './models/todo';
 import NewTodo from './components/NewTodo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript'),
-  ]
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
 
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    })
   }
 
   return (
